c09d82d6ba2f5d64fae037faf5c086a0582ac824
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-07-30 17:25:21 +0530

    feat: add config props

--- src/main/java/com/sivalabs/bookmarks/ApplicationProperties.java
+++ src/main/java/com/sivalabs/bookmarks/ApplicationProperties.java
@@ -5,11 +5,21 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "app")
 public class ApplicationProperties {
     private boolean dataImportEnabled;
+    private int pageSize;
 
     public boolean isDataImportEnabled() {
         return dataImportEnabled;
     }
+
     public void setDataImportEnabled(boolean dataImportEnabled) {
         this.dataImportEnabled = dataImportEnabled;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
     }
 }
